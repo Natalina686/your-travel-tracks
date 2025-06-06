86b3f750897d3d3fb88216e75637c39afd3c1d97
nevada <natalina686@gmail.com>
2025-06-07 00:03:47 +0200

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from './HomePage.module.css';
+import { useNavigate } from "react-router-dom";
 
-function HomePage() {
-    return <h1>Home Page</h1>
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/catalog');
+    };
+
+    return (
+        <section className={styles.home}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Campers of your dreams</h1>
+                <p className={styles.paragraph}>You can find everything you want in our catalog</p>
+                <button className={styles.ctaButton} onClick={handleClick}>
+                    View Now
+                </button>
+
+            </div>
+
+        </section>
+    )
 }
+
+
 
 export default HomePage;
--- src/services/campersApi.js
+++ src/services/campersApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://api.example.com';
+const API_BASE_URL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io';
 
 export function fetchCampers() {
     return axios.get(`${API_BASE_URL}/campers`);
