0ca3fe83e317ec01345c044b41caf782a8d87ffe
nevada <natalina686@gmail.com>
2025-06-07 22:59:12 +0200

    header, burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
+import { NavLink } from "react-router-dom";
 import styles from './Header.module.css';
+import logo from '../../assets/Logo.svg';
 
-function Header() {
-    return <h1 className={styles.header}>Header</h1>
+export default function Header() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+    const closeMenu = () => setMenuOpen(false);
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src={logo} alt="logo" />
+            </div>
+
+
+            <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
+                <NavLink
+                to="/"
+                className={({isActive}) =>
+                isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`
+                }
+                onClick={closeMenu}
+                >
+                    Home
+                </NavLink>
+
+                <NavLink
+                to="/catalog"
+                className={({isActive}) => 
+                isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`
+            }
+            onClick={closeMenu}
+                >
+                    Catalog
+
+                </NavLink>
+
+            </nav>
+
+            <button className={`${styles.burger} ${menuOpen ? styles.open : ""}`}
+            onClick={toggleMenu}
+            aria-label="Toggle menu">
+
+                <span />
+                <span />
+                <span />
+
+            </button>
+
+        </header>
+    
+    )
 }
-export default Header;
